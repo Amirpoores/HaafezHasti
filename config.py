ff17e9a59067fe81200a826db1b3235caedfd5cb
unknown <amirhossein.pooresmaeil@divar.ir>
2025-07-16 20:31:25 +0330

    Initial project structure - Hafez Fal website

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = 'hafez-fal-secret-key'
+    DATABASE_PATH = 'database/hafez.db'
+    UPLOAD_FOLDER = 'static/audio'
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
