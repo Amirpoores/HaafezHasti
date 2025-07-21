8f81e652a393d4c722038b8c5ea55b8ab37eab95
unknown <amirhossein.pooresmaeil@divar.ir>
2025-07-22 00:31:17 +0330

    Fix: Change host to 0.0.0.0 for production deployment

--- app.py
+++ app.py
@@ -151,5 +151,6 @@ def server_error(_):
 
 
 # ------------------------------------------------------------------ #
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
